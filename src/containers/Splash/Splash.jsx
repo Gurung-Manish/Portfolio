6209fb4c22fa63fd5f49b4132c9fb14e3a5df546
Gurung-Manish <manishgrg496@gmail.com>
2024-09-12 22:27:48 +0100

    adding first hello for now

--- src/containers/Splash/Splash.jsx
+++ src/containers/Splash/Splash.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./Splash.css";
 
 const Splash = () => {
-  const startLoader = () => {};
-  return <div class="splash">Hello</div>;
+  return (
+    <div class="splash">
+      <p class="splash_label">Hello</p>
+    </div>
+  );
 };
 
 export default Splash;
