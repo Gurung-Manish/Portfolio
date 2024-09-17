ba74afe14017ecd92cdf6f6c45084c10b4c95ab2
Gurung-Manish <manishgrg496@gmail.com>
2024-09-17 18:13:55 +0100

    layering of picture and text content

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import hero from "../../assets/hero.png";
 import "./Home.css";
 
 const Home = () => {
-  return <div>this is home</div>;
+  return (
+    <div className="home">
+      <div className="home_hero">
+        <img className="home_hero_picture" src={hero} alt="hero"></img>
+      </div>
+      <div className="home_content">
+        <div className="home_content_text">
+          <h3>
+            It's me <br /> Manish Gurung
+          </h3>
+        </div>
+        <marquee>Manish Gurung</marquee>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
