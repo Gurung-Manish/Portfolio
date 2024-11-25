c933cf653fa3a613f37e1bb9cef1b20b23b28c7e
Gurung-Manish <manishgrg496@gmail.com>
2024-11-25 13:04:14 +0000

    redoing the entire home from scratch

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,21 +3,7 @@ import hero from "../../assets/hero.png";
 import "./Home.css";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <div className="home_hero">
-        <img className="home_hero_picture" src={hero} alt="hero"></img>
-      </div>
-      <div className="home_content">
-        <div className="home_content_text">
-          <h3>
-            It's me <br /> Manish Gurung
-          </h3>
-        </div>
-        <marquee>Manish Gurung</marquee>
-      </div>
-    </div>
-  );
+  return <div className="home"></div>;
 };
 
 export default Home;
