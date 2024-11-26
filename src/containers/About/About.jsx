9acb86fe8746eda77eac3b84aa179ca84e567878
Gurung-Manish <manishgrg496@gmail.com>
2024-11-26 17:32:03 +0000

    changing about layout

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./About.css";
 import heroImage from "../../assets/hero.png";
+
 const About = () => {
   return (
     <div className="about" id="about">
+      <div className="about__watermark">ABOUT ME</div>
       <div className="about__body">
         <div className="about__image">
-          <img src={heroImage} alt="Manish Gurung" style={{ width: "100%" }} />
+          <img src={heroImage} alt="Manish Gurung" />
         </div>
         <div className="about__text">
           <h1>Manish Gurung</h1>
           <p>
             Versatile Software Engineer experienced in full-stack development,
-            skilled at delivering scalable, highperformance applications with a
+            skilled at delivering scalable, high-performance applications with a
             focus on backend and frontend integration. Passionate about solving
             complex problems through innovative approaches and working
             collaboratively in Agile teams.
@@ -20,7 +22,6 @@ const About = () => {
           <button className="about__button">Download CV</button>
         </div>
       </div>
-      <div className="about__watermark">ABOUT ME</div>
     </div>
   );
 };
