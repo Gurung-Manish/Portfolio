0f7c9a80c15cd95dfbaa867baca8401a46d7bed2
Gurung-Manish <manishgrg496@gmail.com>
2024-11-29 18:13:49 +0000

    finsihed layout and css of contact

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./Contact.css";
 const Contact = () => {
+  const handleContact = () => {};
+
   return (
     <div className="contact" id="contact">
-      <h1>conts</h1>
+      <h1>Manish Gurung</h1>
+      <p>
+        Wanna bounce off ideas,
+        <br /> feel free to send me an email
+      </p>
+      <button className="contact__button" onClick={handleContact}>
+        Contact
+      </button>
     </div>
   );
 };
